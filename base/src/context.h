84e3948b61cecbc021fb36d54c1e46b6eb82c2c1
yamakiller <296270991@qq.com>
2019-04-24 08:45:00 +0800

    yamakiller

--- base/src/context.h
+++ base/src/context.h
@@ -7,10 +7,39 @@
 
 namespace engine {
 class context {
+
+  context(fn_t fn, intptr_t ud, size_t stackSize)
+      : m_fn(fn), m_ud(ud), m_stackSize(stackSize) {
+    m_stack = (char *)::malloc(m_stackSize);
+    m_ctx = make_fcontext(m_stack + m_stackSize, m_stackSize, m_fn);
+  }
+
+  ~context() {
+    if (m_stack) {
+      ::free(m_stack);
+      m_stack = NULL;
+    }
+  }
+
+  inline void SwapIn() { jump_fcontext(&getTlsContext(), m_ctx, m_ud); }
+
+  inline void SwapTo(context &other) {
+    jump_fcontext(&m_ctx, other.m_ctx, other.m_ud);
+  }
+
+  inline void SwapOut() { jump_fcontext(&m_ctx, getTlsContext(), 0); }
+
+  fcontext_t &getTlsContext() {
+    static thread_local fcontext_t tls_context;
+    return tls_context;
+  }
+
 private:
   fcontext_t m_ctx;
   char *m_stack;
-  uint32_t m_stacksize;
+  fn_t m_fn;
+  intptr_t m_ud;
+  uint32_t m_stackSize;
 };
 } // namespace engine
 
--- base/src/fcontext.h
+++ base/src/fcontext.h
@@ -10,7 +10,29 @@ using boost::context::fcontext_t;
 using boost::context::jump_fcontext;
 using boost::context::make_fcontext;
 
-typedef std::function<void(intptr_t)> fcontext_call;
+/**
+ * @biref 跳转到目标上下文
+ * @param ofc 当前的上下文会保存到ofc中
+ * @param nfc 跳转到的目标上下文
+ * @param vp 跳转到的目标上下文的附加参数。如果是第一次跳转，
+ *          作为函数参数传入，如果是调回到jump_fcontext，这个是返回值
+ * @param preserve_fpu 是否复制FPU（浮点数寄存器）数据
+ * @return 如果调回时的透传参数
+ */
+/*intptr_t BOOST_CONTEXT_CALLDECL jump_fcontext(fcontext_t *ofc, fcontext_t nfc,
+ * intptr_t vp, bool preserve_fpu = false);*/
+
+/**
+ * @biref 初始化执行环境上下文
+ * @param sp 栈空间地址
+ * @param size 栈空间的大小
+ * @param fn 入口函数
+ * @return 返回初始化完成后的执行环境上下文
+ */
+/*BOOST_CONTEXT_DECL fcontext_t BOOST_CONTEXT_CALLDECL
+make_fcontext(void *sp, std::size_t size, void (*fn)(intptr_t));*/
+
+typedef void (*fn_t)(intptr_t);
 
 } // namespace engine
 
