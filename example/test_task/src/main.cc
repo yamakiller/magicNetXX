7e50787132287dd1d05442b7f77d45c751ac88f6
yamakiller <296270991@qq.com>
2019-05-08 17:33:41 +0800

    00

--- example/test_task/src/main.cc
+++ example/test_task/src/main.cc
@@ -4,47 +4,8 @@
 #include <string>
 #include <thread>
 
-//测试数据结构
-#include <deque.h>
-
-struct atask : public engine::util::node, public engine::shared_ref
-{
-    int32_t id;
-    atask()
-    {
-    }
-
-    ~atask()
-    {
-    }
-
-private:
-    atask(atask const &) = delete;
-    atask(atask &&) = delete;
-    atask &operator=(atask const &) = delete;
-    atask &operator=(atask &&) = delete;
-};
-
 int main(int argc, char *argv[])
 {
-
-    engine::util::deque<atask, false> q;
-
-    for (int i = 0; i < 1000; i++)
-    {
-        atask *p = new atask();
-        p->id = 1 + i;
-        q.push(p);
-        engine::decrementRef<atask>(p);
-    }
-
-    fprintf(stderr, "push end\n");
-    q.assertLink();
-
-    auto l = q.popBackAll();
-    auto o = l.cut(10);
-    l.append(std::move(o));
-
     /*1.engine::scheduler::instance()->doStart(6);
 
     while (1)
