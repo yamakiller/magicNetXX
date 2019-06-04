d65752f200e857052db9b5702087192159cfb2c2
yamakiller <296270991@qq.com>
2019-06-04 13:41:38 +0800

    py运行器1.0.0

--- component/SocketTest/src/SocketTest.cc
+++ component/SocketTest/src/SocketTest.cc
@@ -10,8 +10,7 @@ using namespace wolf::util;
 
 #define TEST_MAX_BUFFER 2048
 
-class clientTest : public mobject
-{
+class clientTest : public mobject {
 public:
   int32_t _sock;
   char _addr[64];
@@ -24,21 +23,18 @@ public:
   ~clientTest() = default;
 };
 
-class SocketTest : public actorComponent
-{
+class SocketTest : public actorComponent {
 public:
   SocketTest();
   virtual ~SocketTest();
 
 protected:
-  int32_t onLaunch()
-  {
+  int32_t onLaunch(void *parm) {
     const char *addr = "127.0.0.1";
     const int port = 29810;
     socketApi::doRequire(this);
     int32_t sock = socketApi::doListen(getActorPtr(), addr, port, 1024);
-    if (sock == -1)
-    {
+    if (sock == -1) {
       LOCAL_LOG_ERROR("监听失败:[{}:{}]", addr, port);
       return -1;
     }
@@ -51,8 +47,7 @@ protected:
     return 0;
   }
 
-  void onAccept(int32_t clientId, const char *addr)
-  {
+  void onAccept(int32_t clientId, const char *addr) {
     LOCAL_LOG_INFO("accept socket:{}[{}]", clientId, addr);
     std::shared_ptr<clientTest> p(new clientTest());
     p->_sock = clientId;
@@ -62,16 +57,12 @@ protected:
     p->_addr[strlen(addr)] = '\0';
 
     boost::any ret = socketApi::doStart(getActorPtr(), clientId);
-    if (ret.type() == typeid(int32_t) || ret.type() == typeid(int))
-    {
-      if (boost::any_cast<int>(ret) == -1)
-      {
+    if (ret.type() == typeid(int32_t) || ret.type() == typeid(int)) {
+      if (boost::any_cast<int>(ret) == -1) {
         LOCAL_LOG_ERROR("client create fail:{}[{}]", clientId, addr);
         return;
       }
-    }
-    else
-    {
+    } else {
       LOCAL_LOG_ERROR("client create fail:{},error:{}", clientId,
                       boost::any_cast<std::string>(ret).c_str());
       return;
@@ -83,8 +74,7 @@ protected:
     char *sendinf = (char *)memory::malloc(8);
     strcpy(sendinf, "accept");
     sendinf[6] = '\0';
-    if (socketApi::doSend(clientId, sendinf, 7) != 0)
-    {
+    if (socketApi::doSend(clientId, sendinf, 7) != 0) {
       LOCAL_LOG_ERROR("client send accept info fail:{}[{}]", clientId, addr);
       return;
     }
@@ -93,21 +83,18 @@ protected:
 
     //最佳选择交给另外，ACTOR来处理，需要提供套接字同步锁用协程代替
     LOCAL_LOG_INFO("等待读取数据...");
-    for (;;)
-    {
+    for (;;) {
       ret = socketApi::doRead(clientId, p->_buffer + p->_size,
                               TEST_MAX_BUFFER - p->_size);
       int tmp = boost::any_cast<int>(ret);
-      if (tmp == -1)
-      {
+      if (tmp == -1) {
         removeClient(clientId);
         LOCAL_LOG_INFO("连接被断开:{}[{}]", clientId, addr);
         break;
       }
 
       p->_size += tmp;
-      if (p->_size >= TEST_MAX_BUFFER)
-      {
+      if (p->_size >= TEST_MAX_BUFFER) {
         p->_size = 0;
         LOCAL_LOG_INFO("数据缓冲区已满，清除");
       }
@@ -117,39 +104,32 @@ protected:
   }
 
 private:
-  std::shared_ptr<clientTest> getClient(int32_t id)
-  {
+  std::shared_ptr<clientTest> getClient(int32_t id) {
     std::unique_lock<util::spinlock> lock(m_lock);
-    if (m_clients.empty())
-    {
+    if (m_clients.empty()) {
       return nullptr;
     }
 
     auto it = m_clients.find(id);
-    if (it == m_clients.end())
-    {
+    if (it == m_clients.end()) {
       return nullptr;
     }
 
     return m_clients[id];
   }
-  void addClient(int32_t id, std::shared_ptr<clientTest> ptr)
-  {
+  void addClient(int32_t id, std::shared_ptr<clientTest> ptr) {
     std::unique_lock<util::spinlock> lock(m_lock);
     m_clients[id] = ptr;
   }
 
-  void removeClient(int32_t id)
-  {
+  void removeClient(int32_t id) {
     std::unique_lock<util::spinlock> lock(m_lock);
-    if (m_clients.empty())
-    {
+    if (m_clients.empty()) {
       return;
     }
 
     auto it = m_clients.find(id);
-    if (it == m_clients.end())
-    {
+    if (it == m_clients.end()) {
       return;
     }
     m_clients.erase(it);
@@ -160,22 +140,15 @@ private:
   util::spinlock m_lock;
 };
 
-extern "C" void *
-SocketTest_create()
-{
+WOLF_C_API void *SocketTest_create() {
   SocketTest *ptr = new SocketTest();
   assert(ptr);
   return (void *)ptr;
 }
 
-extern "C" void
-SocketTest_signal(void *inst, int signal)
-{
-}
+WOLF_C_API void SocketTest_signal(void *inst, int signal) {}
 
-extern "C" void
-SocketTest_release(void *inst)
-{
+WOLF_C_API void SocketTest_release(void *inst) {
   SocketTest *ptr = (SocketTest *)inst;
   delete ptr;
 }
