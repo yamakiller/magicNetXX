c03e0a9bf02ca50f11b4253307502255b1864015
yamakiller <296270991@qq.com>
2019-06-12 10:31:31 +0800

    修改兼容lua5.1编译

--- engine/src/lualib/manual/luaExternApi.h
+++ engine/src/lualib/manual/luaExternApi.h
@@ -3,13 +3,5 @@
 
 #include <lua.hpp>
 #include "luaFix.h"
-#if LUA_VERSION_NUM > 501
-#define luaL_register(l, name, funs) \
-    lua_newtable(l);                 \
-    luaL_setfuncs(l, funs, 0);       \
-    lua_pushvalue(l, -1);            \
-    lua_setglobal(l, name);
-#else
-#endif
 
 #endif
